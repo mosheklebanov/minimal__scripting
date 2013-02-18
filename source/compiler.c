f81ced61c91e6b179282f8a88c775a4cac907d54
Moshe Klebanov <moshe_klebanov@yahoo.com>
2013-02-18 00:47:43 -0700

    Cleaned up macro definitions; added macro SORT_TOKENS()

--- source/compiler.c
+++ source/compiler.c
@@ -5,7 +5,7 @@ Token definition and typedef
 Tokens will be gathered for each statement, then evaluated in descending order of depth
 */
 #define TOKEN_QUANTITY_PER_STMT 24
-typedef struct token_t
+typedef struct token_t /* by reference */
 {
 	unsigned chr_pos;
 	unsigned depth;
@@ -36,25 +36,27 @@ The following variables MUST be avaliable in the local scope of the calling func
 	unsigned chr;
 	unsigned line;
 	unsigned traversal_depth;
-	unsigned token_i;
-	unsigned token_d;
+	unsigned token_quantity;
 	TOKEN tokens[n];
 	
 */
-#define MOVE_NEXT_LINE() \
+#define MOVE_NEXT_LINE() do { \
 	if (chr>=src_len && traversal_depth>0) err(chr, line, IN_BLOCK_NO_MORE_CODE); \
 	if (chr>=src_len) return; /*stop parsing*/ \
 	for (;chr<src_len && src[chr]!=NEWL; chr++) {} /*otherwise, move to the next line and put the cursor there*/ \
+} while(0)
 
-#define GATHER_TOKENS() \
-	for (token_i=0, token_d=0, chr2=chr; chr2<src_len && chr2!=NEWL; chr2++) \
+#define GATHER_TOKENS() do { \
+	unsigned chr2; \
+	unsigned token_d=0; \
+	for (chr2=chr; chr2<src_len && src[chr2]!=NEWL; chr2++) \
 	{ \
 		if (IS_OPERATOR(src[chr2])) \
 		{ \
-			tokens[token_i].chr_pos = chr2; \
-			tokens[token_i].depth = token_d; \
-			token_i++; \
-			if (token_i>=TOKEN_QUANTITY_PER_STMT) \
+			tokens[token_quantity].chr_pos = chr2; \
+			tokens[token_quantity].depth = token_d; \
+			token_quantity++; \
+			if (token_quantity>=TOKEN_QUANTITY_PER_STMT) \
 			{ \
 				err(chr2, line, TOO_MANY_TOKENS); \
 				break; \
@@ -62,17 +64,31 @@ The following variables MUST be avaliable in the local scope of the calling func
 		} \
 		else if (src[chr2]=='(') token_d++; \
 		else if (src[chr2]==')') token_d--; \
-	}
-		
-		
+	} \
+} while(0)
+
+#define SORT_TOKENS() do { \
+	TOKEN t; \
+	unsigned i=0; \
+	for (;i<(token_quantity-1); i++) \
+	{ \
+		if (tokens[i].depth<tokens[i+1].depth) \
+		{ \
+			t = tokens[i]; \
+			tokens[i] = tokens[i+1]; \
+			tokens[i+1] = t; \
+		} \
+	} \
+} while(0)
+	
 
 //credits: http://www.cse.yorku.ca/~oz/hash.html
-unsigned long hash(char* s)
+unsigned long hash(char* s, unsigned len)
 {
 	unsigned long hashval = 0;
-	int c;
-	while (c = *s++)
-		hashval = c + (hashval<<6) + (hashval<<16) - hashval;
+	unsigned c;
+	for (c=0;c<len;c++)
+		hashval = s[c] + (hashval<<6) + (hashval<<16) - hashval;
 	return hashval;
 }
 
@@ -90,16 +106,15 @@ as long as (entire) evaluates to false
 */
 void gen_bytecode(const char* src, unsigned src_len, int entire, CALLBACK cb, ERROR err)
 {
-	unsigned chr = 0; /* persistent; state should be preserved, except in MOVE_* macros */
-	unsigned chr2 = 0; /* non-persistent; may be freely modified by any macros */
+	unsigned chr = 0;
 	unsigned line = 1;
 	unsigned traversal_depth = 0;
-	unsigned token_i = 0;
-	unsigned token_d = 0;
+	unsigned token_quantity = 0;
 	TOKEN tokens[TOKEN_QUANTITY_PER_STMT];
 	while (1)
 	{
-		MOVE_NEXT_LINE()
-		GATHER_TOKENS()
+		MOVE_NEXT_LINE();
+		GATHER_TOKENS();
+		SORT_TOKENS();
 	}
 }
